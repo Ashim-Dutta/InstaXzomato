ff8ee7ab0c6618aec64919ecf9507c3378c35963
Ashim Dutta <ashimdutta800@gmail.com>
2025-09-06 01:12:15 +0530

    setup

---
+++ frontend/src/App.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const App = () => {
+  return (
+    <div className='text-6xl font-bold bg-green-200 flex justify-center items-center h-screen'>
+      <h1>Insta X zomato</h1>
+    </div>
+  )
+}
+
+export default App
